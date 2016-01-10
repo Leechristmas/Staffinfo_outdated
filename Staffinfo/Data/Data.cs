847752c94377813b0c6400cae90b929479c4235b
Dmitry Shevchuk <Dmitry Shevchuk>
2016-01-10 16:35:56 +0300

    Initial Commit

---
+++ Staffinfo/Data/Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staffinfo.Data
+{
+    public class Data
+    {
+        private static Data _instance;
+
+        private Data()
+        {
+
+        }
+
+        public static Data Instance
+        {
+            get { return _instance ?? (_instance = new Data()); }
+        }
+
+
+
+
+    }
+}
