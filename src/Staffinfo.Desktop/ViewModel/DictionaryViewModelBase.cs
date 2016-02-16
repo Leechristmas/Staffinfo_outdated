9788b0a5df3b2dadc8f9311a3c28943d4b977fe1
Leechristmas <christmas.stealth@gmail.com>
2016-02-16 18:18:12 +0300

    Добавил view для добавления паспортного стола, учебного заведения, специальности, воинской части

---
+++ src/Staffinfo.Desktop/ViewModel/DictionaryViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Staffinfo.Desktop.ViewModel
+{
+    /// <summary>
+    /// Базовый класс для view-моделей справочников
+    /// </summary>
+    public abstract class DictionaryViewModelBase: WindowViewModelBase
+    {
+        /// <summary>
+        /// Название справочника
+        /// </summary>
+        public string DirectoryTitle { get; private set; }
+    }
+}
