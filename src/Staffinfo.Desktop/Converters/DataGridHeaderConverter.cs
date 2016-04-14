02ba7ae32b9a8247e9abd5d940a20c50df29b613
Leechristmas <christmas.stealth@gmail.com>
2016-04-14 18:58:04 +0300

    forced commit: заготовки для реализации отображения справочной информации

---
+++ src/Staffinfo.Desktop/Converters/DataGridHeaderConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Staffinfo.Desktop.Properties;
+
+namespace Staffinfo.Desktop.Converters
+{
+    /// <summary>
+    /// Конвертирует header'ы для data grid
+    /// </summary>
+    public class DataGridHeaderConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var stringValue = value as string;
+            if (stringValue == null) return null;
+
+
+            return Resources.ResourceManager.GetString(stringValue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
